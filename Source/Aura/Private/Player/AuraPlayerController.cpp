02fafdfcfd2f38cfab4ef566dcb37363cc115355
unknown <1169762539@qq.com>
2024-06-27 02:01:24 +0800

    add an enhancedInputLocalPlayerSubsystem

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,31 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+#include "Layers/Layer.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	// 不将鼠标锁定到视口
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	// 一旦视口获取到鼠标，不会隐藏鼠标
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
